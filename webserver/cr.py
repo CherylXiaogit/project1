52fd2b26adccb841e711947527bedb782035b1da
CherylXiaogit <yumengmeng.xiao@gmail.com>
2018-11-26 21:27:28 -0500

    Update cr.py

--- webserver/cr.py
+++ webserver/cr.py
@@ -127,9 +127,9 @@ SIGNUP_USER = '''INSERT INTO users (uid, school_name, contact_info)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	username = request.form['username']
-       school_name = request.form["school_name"]
-      contact_info = request.form["contact_info"]
+	uid = request.form['uid']
+	school_name = request.form["school_name"]
+	contact_info = request.form["contact_info"]
         r = g.conn.execute('select uid from users')
         # if username already exists
         res = [re for re in r]
