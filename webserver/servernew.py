c0f56a73bdd1c3d45726083b64955b9991745ffc
CherylXiaogit <yumengmeng.xiao@gmail.com>
2018-11-27 17:02:26 -0500

    Update servernew.py

--- webserver/servernew.py
+++ webserver/servernew.py
@@ -136,7 +136,7 @@ def login():
       result = [r for r in cursor]
       print(result)
       if  len(result) != 0:
-        resp = make_response(redirect("/"))
+        resp = make_response()
         delete_cookie(resp)
         resp.set_cookie('uid', uid)
         return resp, render_template('user_page.html')
