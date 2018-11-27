bb688dd8daf76b70b8d155e4d9eeda2896393b1b
CherylXiaogit <yumengmeng.xiao@gmail.com>
2018-11-27 15:39:44 -0500

    Update cr.py

--- webserver/cr.py
+++ webserver/cr.py
@@ -161,7 +161,9 @@ def all_items():
     
 @app.route('/myitem')
 def myitem():
-	
+	if request.method == "GET":
+		return render_template("myitem.html")
+	else:
 		uid = request.form["uid"]
         	cursor = g.conn.execute('select * from item where uid =\'' + uid + '\' ')
         	info = []
